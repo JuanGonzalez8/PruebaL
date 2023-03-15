1f7fe5c70101d591e088b81917e80ac4030fa855
JuanGonzalez8 <e18081088@itmerida.edu.mx>
2023-03-15 09:11:05 -0600

    ionic start project

---
+++ Login/src/app/pages/pages-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PagesPage } from './pages.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesPage
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesPageRoutingModule {}
